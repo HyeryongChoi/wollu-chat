eae3a045561dff7aa4bf76556d9b3240cf1d8067
HyeryongChoi <chex1004@gmail.com>
2024-02-28 14:52:26 +0900

    chore: 프로젝트 초기 설정
    - react
    - typescript
    - vite
    - prettier
    - tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Spreadsheet from '@components/Spreadsheet/Spreadsheet';
+
+function App() {
+  return (
+    <div className='w-full'>
+      <Spreadsheet />
+    </div>
+  );
+}
+
+export default App;
