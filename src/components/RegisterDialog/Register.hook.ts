90ba584f09a1a25d0923238c6cc1dce0228bfd33
HyeryongChoi <chex1004@gmail.com>
2024-03-03 01:09:50 +0900

    feat: 회원가입 요청 훅 생성

---
+++ src/components/RegisterDialog/Register.hook.ts
@@ -0,0 +1,19 @@
+import { useDialogContext } from '@components/@common/Dialog/DialogContext';
+
+import { useMutation } from '@hooks/@common/useMutation';
+
+import { postUser } from '@apis/user/postUser';
+
+export const useRegisterMutation = () => {
+  const { closeDialog } = useDialogContext();
+
+  const { mutate, ...restMutation } = useMutation({
+    mutationFn: postUser,
+    onSuccess: () => {
+      closeDialog();
+      alert('회원가입이 완료되었습니다.');
+    },
+  });
+
+  return { register: mutate, restMutation };
+};
